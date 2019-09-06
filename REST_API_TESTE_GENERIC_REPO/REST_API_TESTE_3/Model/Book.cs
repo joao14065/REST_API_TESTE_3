3aa5bd258ce19da6a3eaa2f6b94cbd66d6351069
joao <gabriel@alfredddelivery.com>
2019-09-06 18:40:02 -0300

    Rest API with generic repository

---
+++ REST_API_TESTE_GENERIC_REPO/REST_API_TESTE_3/Model/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace REST_API_TESTE_3.Model
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Authos { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LaunchDate { get; set; }
+    }
+}
---
+++ REST_API_TESTE_GENERIC_REPO/REST_API_TESTE_3/Repository/IPersonRepository.cs
@@ -0,0 +1,19 @@
+﻿using REST_API_TESTE_3.Model;
+using System.Collections.Generic;
+
+namespace REST_API_TESTE_3.Repository
+{
+    // Serviço do objeto 'Person'
+    // Tal serviço realiza todas as possíveis ações que o objeto pode realizar
+    // Aconselha-se a criar um serviço padrão p/ todos os objetos utilizados em soluções futuras
+    public interface IPersonRepository
+    {
+        Person Create(Person person);
+        Person FindById(long Id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long Id);
+        bool Exist(long id);
+
+    }
+}
